c96b7ae9a475e1f6d5216fb3bd23e7217f5c23ed
mariamsuluahvili <140321036+mariamsuluahvili@users.noreply.github.com>
2026-02-25 12:45:22 +0400

    Add files via upload
    1/2_leacture

---
+++ gau_php-mysql_2026_ms/leacture2/test.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<h1> lecture 2 </h2>";
+    $arr_1 = [34,5.8,"gau",False,"i am a students"];
+    echo "<pre>";
+    print_r($arr_1);
+    echo "<pre>";
+    $arr_asoc=['name'=>"mariami",'age'=>21,"subject"=>"php web technologies"];
+    echo "<pre>";
+    print_r($arr_asoc);
+    echo "<pre>";
