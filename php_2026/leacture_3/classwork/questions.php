4eb8d6f2e32510e3efb21c90f0860f2614b383e9
mariamsuluahvili <140321036+mariamsuluahvili@users.noreply.github.com>
2026-03-04 01:00:32 -0800

    leacture_3

---
+++ php_2026/leacture_3/classwork/questions.php
@@ -0,0 +1,12 @@
+<?php
+$questions=[
+['question'=>"what is js?",'max_point'=>8],
+['question'=>"what is react?",'max_point'=>10],
+['question'=>"what is angular?",'max_point'=>10],
+['question'=>"what is html?",'max_point'=>7],
+['question'=>"what is php?",'max_point'=>8],
+['question'=>"what is css?",'max_point'=>9],
+
+];
+
+?>
