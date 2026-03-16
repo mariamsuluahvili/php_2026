baa9c2f29efc1eff15645b4a950d04e7039d498d
mariamsuluahvili <140321036+mariamsuluahvili@users.noreply.github.com>
2026-03-16 10:47:58 -0700

    praktikuli2

--- leacture_4/classwork/form.php
+++ leacture_4/classwork/form.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lecture 4</title>
-    <link rel="stylesheet" href="style.css">
+   <style>
+    table, td, th {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 7px;
+            text-align: center;
+        }
+   </style>
 </head>
 <body>
     <form method="post">
@@ -12,40 +19,78 @@
         <h1>PHP Form Validation Example</h1>
         Name: <input type="text" name="name" value="<?php if(isset($_POST['name'])) echo $_POST['name']; ?>"> - *
         <br><br>
-        E-mail:<input type="text" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>">
+        E-mail:<input type="text" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>">
         <br><br>
-        Website: <input type="text" name="website">
+        Website: <input type="text" name="website" value="<?php if(isset($_POST['website'])) echo $_POST['website']; ?>">
         <br><br>
-        Comment: <textarea name="comment"></textarea>
+        Comment: <textarea name="comment"><?php if(isset($_POST['comment'])) echo $_POST['comment']; ?></textarea>
         <br><br>
-        Gender: <input type="radio" name="gender">Famale
-                <input type="radio" name="gender">Male
+        Gender: <input type="radio" name="gender" value="Female">Female
+                <input type="radio" name="gender" value="Male">Male
         <br><br>
         <input type="submit" name="submit-form">  
     </form>
 
     <div class="result">
         <?php
-            echo "<pre>";
-            print_r($_POST);
-            echo "</pre>";
             if(isset($_POST['submit-form'])){
-                echo "<h3>clicked</h3>";
                 if(empty($_POST['name']))
                 {
                     echo "<p>Name is required</p>";
+                }
+                  if(strlen($_POST['name'])<2)
+                {
+                    echo "<p>name is not valid</p>";
                 }
              
                 if(empty($_POST['email']))
                 {
                     echo "<p>E-mail is required</p>";
                 }
+                   if(!str_contains($_POST['email'], "@"))
+                {
+                    echo "<p>E-mail is not valid</p>";
+                }
                 if(empty($_POST['website']))
                 {
                     echo "<p>Website is required</p>";
                 }
-            }
+                  if(empty($_POST['website']))
+                {
+                    echo "<p>Website is required</p>";
+                }
+                 if(empty($_POST['comment']))
+                {
+                    echo "<p>Comment is required</p>";
+                }
+                  if(empty($_POST['gender']))
+                {
+                    echo "<p>gender is required</p>";
+                }
 
+         else {  
+            ?>
+         <table>
+            <tr>
+                <th>Name</th>
+                <th>E-mail</th>
+                <th>Website</th>
+                <th>Comment</th>
+                <th>Gender</th>
+            </tr>
+            <tr>
+                <td><?php echo $_POST['name']; ?></td>
+                <td><?php echo $_POST['email']; ?></td>
+                <td><?php echo $_POST['website']; ?></td>
+                <td><?php echo $_POST['comment']; ?></td>
+                <td><?php echo $_POST['gender']; ?></td>
+            </tr>
+         </table>
+
+         <?php  } ?>
+
+        <?php
+            }
         ?>
     </div>
     <?php 
--- leacture_4/homework/praqtikuli2/savarjisho1.php
+++ leacture_4/homework/praqtikuli2/savarjisho1.php
@@ -1,56 +1,63 @@
+<?php
+session_start(); 
+
+if(!isset($_SESSION['masivi'])) {
+    $_SESSION['masivi'] = [];
+    for($i=0;$i<12;$i++){
+        $_SESSION['masivi'][] = rand(10,100);
+    }
+}
+
+$masivi = $_SESSION['masivi'];
+
+$meti = 0;
+$naklebi = 0;
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $number = $_POST['number'];
+
+    foreach($masivi as $value){
+        if($value > $number){
+            $meti++;
+        } elseif($value < $number){
+            $naklebi++;
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-       form {
-    display: flex;             
-    flex-direction: column;   
-    align-items: center;       
-    justify-content: center;   
-    gap: 10px;                 
-    height: 200px;             
-}
-    </style>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Matrix Example</title>
+<style>
+   form {
+        display: flex;             
+        flex-direction: column;   
+        align-items: center;       
+        justify-content: center;   
+        gap: 10px;                 
+        height: 200px;             
+    }
+</style>
 </head>
 <body>
-    <form action="" method="post">
-   <h2>sheikvane ricxvi</h2>  
-  <input type="" name="number" >
-    <br>
-    <button>submit</button>
-
+<form action="" method="post">
+   <h2>შეიყვანე რიცხვი</h2>  
+   <input type="number" name="number" value="<?php if(isset($_POST['number'])) echo $_POST['number']; ?>">
+   <br>
+   <button>submit</button>
 </form>
+
+
+<p>მასივი: <?= implode(" ", $masivi); ?></p>
+
+
+<?php if($_SERVER['REQUEST_METHOD']=="POST"){ ?>
+    <p>მეტი: <?= $meti ?></p>
+    <p>ნაკლები: <?= $naklebi ?></p>
+<?php } ?>
 </body>
-</html> 
-
-<?php
-if($_SERVER['REQUEST_METHOD']=="POST"){
-$number=$_POST['number'];
-$masivi=[];
-for($i=0;$i<12;$i++){
-    $masivi[]=rand(10,100);
-
-}
-$meti=0;
-$naklebi=0;
-foreach($masivi as $value){
-    if($value>$number){
-    $meti++;
-    }
-    elseif($value<$number){
-        $naklebi++;
-    }
-   
-}
-
-echo "masivi:";
-echo implode (", ", $masivi);
-echo "<br>";
-echo "meti: $meti";
-echo "<br>";
-echo "naklebi: $naklebi";
-}
-?>
+</html>
