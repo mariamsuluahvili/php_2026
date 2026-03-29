7edd1df9987904d8df2f395eb981349d6e74a69e
mariamsuluahvili <140321036+mariamsuluahvili@users.noreply.github.com>
2026-03-29 13:12:32 +0400

    leacture6

---
+++ Lecture_6/page.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture 6</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="file.php" method="get">
+        File Name - <input type="text" name="f-name">
+        <br><br>
+        <button>Create File</button>
+    </form>
+    <hr>
+    <form action="file1.php" method="get">
+        File Name - <input type="text" name="f-name">
+        <br><br>
+        File Content - <textarea name="f-content"></textarea>
+        <br><br>
+        <button>Write to File</button>
+    </form>
+    <hr>
+    <div class="content">
+        <h1>List of Files1</h1>
+        <?php
+            $d = scandir("files1");
+            // echo "<pre>";
+            // print_r($d);
+            // echo "</pre>";
+
+            for($i=2; $i<count($d); $i++){
+                echo "<p>$d[$i]</p>";
+            }
+        ?>
+    </div>
+</body>
+</html>
