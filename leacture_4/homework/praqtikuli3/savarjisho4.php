8e9cc9b4a0c598c1fa41071c99a420f9269cd8de
mariamsuluahvili <140321036+mariamsuluahvili@users.noreply.github.com>
2026-03-29 02:17:16 -0700

    .

--- leacture_4/homework/praqtikuli3/savarjisho4.php
+++ leacture_4/homework/praqtikuli3/savarjisho4.php
@@ -1,47 +1,101 @@
-<?php
-// პაროლის სიმძლავრის განსაზღვრა
-function passwordStrength($password) {
-    $length = strlen($password);
-    $strength = 0;
-
-    if ($length < 6) return "სუსტი";
-
-    if ($length >= 6) $strength++;
-    if (preg_match('/[a-z]/', $password)) $strength++;
-    if (preg_match('/[A-Z]/', $password)) $strength++;
-    if (preg_match('/[0-9]/', $password)) $strength++;
-    if (preg_match('/[\W]/', $password)) $strength++;
-
-    if ($strength <= 2) return "სუსტი";
-    elseif ($strength <= 4) return "საშუალო";
-    else return "მძლავრი";
-}
-
-$result = "";
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $password = $_POST['password'] ?? '';
-    $result = passwordStrength($password);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Password Strength Checker</title>
+    <title>Matrix</title>
+    <style>
+        table, td {
+            border:1px solid black;
+            border-collapse:collapse;
+        }
+        td{
+            padding:5px;
+            text-align:center;
+        }
+    </style>
 </head>
 <body>
 
-<h2>შეიყვანეთ პაროლი</h2>
-
-<form method="post">
-    <input type="password" name="password" required>
-    <input type="submit" value="შეამოწმე">
+<form method="POST">
+    <input type="text" name="M" placeholder="input M">
+    <input type="text" name="N" placeholder="input N">
+    <input type="text" name="a" placeholder="input a">
+    <input type="text" name="b" placeholder="input b">
+    <button name="submit_click">create matrix</button>
 </form>
 
+<br><br>
+
 <?php
-if ($result) {
-    echo "<p>პაროლის სიმძლავრე: <b>$result</b></p>";
+
+function creatematrix($M,$N,$a,$b){
+    $matrica = array();
+    for($i=0;$i<$M;$i++){
+        for($j=0;$j<$N;$j++){
+            $matrica[$i][$j] = rand($a,$b);
+        }
+    }
+    return $matrica;
+}
+
+if(isset($_POST['submit_click'])){
+
+    $M = $_POST['M'];
+    $N = $_POST['N'];
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+
+    if(empty($M) || empty($N) || empty($a) || empty($b)){
+        ?>
+        <p style="color:red;">ველის შევსება აუცილებელია</p>
+        <?php
+    }
+    else if(!is_numeric($M) || !is_numeric($N) || !is_numeric($a) || !is_numeric($b)){
+        ?>
+        <p style="color:red;">აუცილებლად რიცხვი უნდა იყოს</p>
+        <?php
+    }
+    else{
+
+        $matrica = creatematrix($M,$N,$a,$b);
+
+  
+        $colSum = [];
+        for($j=0; $j<$N; $j++){
+            $colSum[$j] = 0;
+        }
+        ?>
+
+        <table>
+
+        <?php for($i=0;$i<$M;$i++){ 
+            $rowSum = 0;
+        ?>
+            <tr>
+                <?php for($j=0;$j<$N;$j++){ 
+                    $rowSum += $matrica[$i][$j];
+                    $colSum[$j] += $matrica[$i][$j];
+                ?>
+                    <td><?php echo $matrica[$i][$j]; ?></td>
+                <?php } ?>
+
+                
+                <td><b><?php echo $rowSum; ?></b></td>
+            </tr>
+        <?php } ?>
+
+      
+        <tr>
+            <?php for($j=0;$j<$N;$j++){ ?>
+                <td><b><?php echo $colSum[$j]; ?></b></td>
+            <?php } ?>
+            <td></td>
+        </tr>
+
+        </table>
+
+        <?php
+    }
 }
 ?>
 
